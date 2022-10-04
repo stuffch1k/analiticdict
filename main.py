c6beb8d602052fcadab6b55c4c3e40a8c2f72eb5
stuffch1k <elizaveta.sineva@urfu.me@>
2022-10-04 20:39:38 +0500

    dict_with_avg-salary_withrur

--- main.py
+++ main.py
@@ -26,12 +26,25 @@ for row in filtered:
         dic[titles[i]]=ans
     dic_vacancy.append(dic)
 #dic_vacancy['key_skills'] это либо массив строк, либо строка
-#в некоторых строках есть траблы: тк сплитили изначально по \n, key_skills остался неотработан по точкам/двоеточиям
+#в некоторых строках есть траблы: тк сплитили изначально по \n, key_skills остался неотработан по другим знакам
 #нужно сплитить еще раз или это одно значение key_skills?
+#ок препдоложим это все один скилл, сплитить больше не будем
 new_dic_vacancy = []
+#новый словарь где не валютные вакансии
 for i in range(len(dic_vacancy)):
     for key, value in dic_vacancy[i].items():
-        if type(dic_vacancy[i]['key_skills']).__name__ != 'list':
-            print(f'{key}: {value}')
+        if key=='salary_currency' and value=='RUR':
+            new_dic_vacancy.append(dic_vacancy[i])
+#добавим информацию о средней зп. тк размер менять нельзя, храним в ячейке currency
+#rename key?
+for i in range(len(dic_vacancy)):
+    for key, value in dic_vacancy[i].items():
+        dic_vacancy[i]['salary_currency']=math.floor((int(dic_vacancy[i]['salary_from'])+int(dic_vacancy[i]['salary_to']))/2)
+
+#cheking
+# for i in range(len(dic_vacancy)):
+#     for key, value in dic_vacancy[i].items():
+#         if type(dic_vacancy[i]['key_skills']).__name__ != 'list':
+#             print(f'{key}: {value}')
     #print()
 print('OK')
