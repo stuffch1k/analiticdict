9d1fb81ef847d9d0072abd1ea526ec2899607aa9
stuffch1k <elizaveta.sineva@urfu.me@>
2022-10-04 20:25:07 +0500

    key_skills_trouble

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import heapq
 
 file_name="C:\\vacanc.csv"
+#file_name=input()
 File=open(file_name, encoding='utf_8_sig')
 reader=[row for row in csv.reader(File)]
 titles=reader.pop(0)
@@ -24,6 +25,13 @@ for row in filtered:
             ans=clear(row[i])
         dic[titles[i]]=ans
     dic_vacancy.append(dic)
-
+#dic_vacancy['key_skills'] это либо массив строк, либо строка
+#в некоторых строках есть траблы: тк сплитили изначально по \n, key_skills остался неотработан по точкам/двоеточиям
+#нужно сплитить еще раз или это одно значение key_skills?
 new_dic_vacancy = []
+for i in range(len(dic_vacancy)):
+    for key, value in dic_vacancy[i].items():
+        if type(dic_vacancy[i]['key_skills']).__name__ != 'list':
+            print(f'{key}: {value}')
+    #print()
 print('OK')
